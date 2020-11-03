2a20eeed9fffa0cc8ed912439f16b261e7e955e2
HyeonWuJeon <tkawnd111@gmail.com>
2020-11-03 23:03:15 +0900

    기능완성

--- src/App.vue
+++ src/App.vue
@@ -10,9 +10,12 @@
     >
     <!-- <button @click="addTodo">Click</button>  -->
     <hr>
-    <Todo v-for="todo in todos" 
+    <!-- Todo  컴포넌트를 사용하고  todos의 데이터를 뿌려준다 / 자식데이터를 가져온다. -->
+    <Todo v-for="todo in todos"  
     :key="todo.id"
     :todo="todo"
+    @toggle-checkbox ="toggleCheckbox"
+    @click-delete = "deleteTodo"
     />
       
   </div>
@@ -36,6 +39,14 @@ export default {
   },
 
   methods:{
+    deleteTodo(id) {
+      // const index = this.todos.findIndex(todo => {
+      //   return todo.id ===id;
+      // });
+      // this.todos.splice(index, 1); // 삭제할 인덱스, 1개
+      this.todos = this.todos.filter(todo => todo.id !== id); // 입력값을 제외한 나머지값을 배열에 채워준다.
+
+    },
     addTodo(e) {
       console.log(e.target.value);
       this.todos.push({
@@ -44,6 +55,14 @@ export default {
         checked: false
       });
       this.todoText='';
+    },
+    
+    toggleCheckbox({id, checked}) { //구조분해
+      console.log(id, checked);
+      const index = this.todos.findIndex(todo => { //  todos 배열에서 id 인덱스가 같은걸 찾아준다.
+        return todo.id ===id;
+      });
+      this.todos[index].checked = checked; // 찾은값을 바꾼값으로 변경시킨다.
     }
   }
 }
